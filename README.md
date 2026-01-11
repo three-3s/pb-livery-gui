2306ac8917a18c33a6599d007335c25b0942be05
Three <mmphoenix2k@yahoo.com>
2026-01-11 12:30:20 -0600

    initial hello-world stub mod

---
+++ README.md
@@ -0,0 +1,15 @@
+![workshop_preview.png](workshop_preview.png)
+
+
+
+## Overview
+
+This mod provides an in-game GUI for adjusting mech livery parameters. TODO FULLER FEATURES DESC.
+
+Steam Workshop page: https:// TODO
+
+
+
+## Questions & Answers
+
+TODO
---
+++ source/ModLink.cs
@@ -0,0 +1,62 @@
+﻿using Entitas;
+using HarmonyLib;
+using PhantomBrigade;
+using PhantomBrigade.Data;
+using PhantomBrigade.Mods;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+using System;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+// INTRODUCTION / USAGE NOTES:
+//  - The project's reference to 'System' must point to the Phantom Brigade one, not Microsoft.
+//    It was necessary to add
+//    C:\Program Files(x86)\Steam\steamapps\common\Phantom Brigade\PhantomBrigade_Data\Managed\
+//    to the project's Reference Paths, which unfortunately isn't stored in the .csproj.
+//     - (and add reference to UnityEngine.JSONSerializeModule to the project)
+//  - Debug.Log goes to LocalLow/Brace.../.../Player.log
+//  - Harmony.Debug = true + FileLog.Log (and FlushBuffer) goes to desktop harmony.log.txt
+//  - You may want to read more about (or ask a chatbot about):
+//     - How to use eg dnSpy to decompile & search the Phantom Brigade C# module assemblies.
+//     - General info about the 'Entitas' Entity Component System.
+//     - Explain what the HarmonyPatch things are.
+//  - Note that modding this game via C# has some significant overlap with some other heavily
+//    modded games such as RimWorld (another Unity game (+HarmonyLib)).
+//  - Other basic getting-started info:
+//     - https://github.com/BraceYourselfGames/PB_ModSDK/wiki/Mod-system-overview#libraries
+//     - https://wiki.braceyourselfgames.com/en/PhantomBrigade/Modding/ModSystem
+//
+
+namespace ModExtensions
+{
+    //==================================================================================================
+    // (Having a class derived from ModLink might (?) be necessary, but the overrides are probably just
+    //  leftover 'hello world' stuff at this point.)
+    public class ModLinkCustom : ModLink
+    {
+        public static ModLinkCustom ins;
+
+        public override void OnLoadStart()
+        {
+            ins = this;
+            //Debug.Log($"OnLoadStart");
+        }
+
+        public override void OnLoad(Harmony harmonyInstance)
+        {
+            base.OnLoad(harmonyInstance);
+            Debug.Log($"OnLoad | Mod: {modID} | Index: {modIndexPreload} | Path: {modPath}");
+        }
+    }//class
+
+    //+================================================================================================+
+    //||                                                                                              ||
+    //+================================================================================================+
+    public class Patches
+    {
+        //todo
+    }//class Patches
+}//namespace
